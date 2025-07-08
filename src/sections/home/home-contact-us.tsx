e66272fae19390d4352b9464b098e10bba5bdc74
Mubashir313ak <muhammadmubashirak329@gmail.com>
2025-07-08 19:13:43 +0500

    home contact

--- src/sections/home/home-contact-us.tsx
+++ src/sections/home/home-contact-us.tsx
@@ -49,7 +49,6 @@ const HomeContactUs: React.FC = () => {
         justifyContent="center"
         direction={{ xs: 'column', md: 'row' }}
       >
-        {/* Left: Image */}
         <Grid item xs={12} md={6}>
           <Box
             component="img"
@@ -66,7 +65,6 @@ const HomeContactUs: React.FC = () => {
           />
         </Grid>
 
-        {/* Right: Form */}
         <Grid item xs={12} md={6}>
           <Card
             sx={{
